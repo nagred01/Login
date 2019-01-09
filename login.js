89128b8a4045a41233b6a16951380738620c53f4
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-09 11:35:52 -0800

    Update login.js

--- login.js
+++ login.js
@@ -63,7 +63,7 @@
                                             componentState.setState({ password: val })
                                         },
                                          autoCapitalize : 'none',
-                                         secureTextEntry :'true' ,
+                                         secureTextEntry :"true" ,
                                         placeHolder: "Enter the Password"
                                     }, [])
                                 ]),
