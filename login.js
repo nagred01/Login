d011abbc04caed460fe6385cd901ed85eccb7daf
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-20 16:50:59 +0530

    Update login.js

--- login.js
+++ login.js
@@ -74,9 +74,8 @@
                                     "id": "M_layout_content_PCDZ_MNS7LAN_ctl00_btnCancel",
                                     "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_btnCancel",
                                     style : styles.loginButton,
-				   onPress: {()=>{
-                                           FuncCall();
-					    }
+				   onPress: {
+                                           FuncCall()
                                     }
                                 },[react_1.createElement(_reactNative.Text, {
                                     "htmlFor": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl023",
