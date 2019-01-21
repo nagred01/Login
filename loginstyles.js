69c8008bb1688bad08dc4a5441ade5828c53d081
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-21 15:18:11 +0530

    Update loginstyles.js

--- loginstyles.js
+++ loginstyles.js
@@ -68,8 +68,14 @@
         justifyContent:'center'
 
     },
-    modalView:{
-     top:'40%'
-    }
+    activityIndicatorWrapper: {
+    backgroundColor: '#FFFFFF',
+    height: 100,
+    width: 100,
+    borderRadius: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  }
 });
 });
