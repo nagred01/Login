6164d70aae0d3ae12ff0921b844ad5e015685510
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-21 15:56:31 +0530

    Update login.js

--- login.js
+++ login.js
@@ -137,7 +137,7 @@ function validateUser() {
                                     "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl023_Label",
                                     style : styles.loginButtonLabel,
                                 }, ["Login"])]),
-			react_1.createElement(_overlayProgress,{
+			react_1.createElement(_overlayProgress.Overlay,{
 			       "id": "M_layout_content_PCDZ_MNS7LAN_ctl00_mdlCancel",
                                "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_mdlCancel",
 				 visible:componentState.state.progressModal,
