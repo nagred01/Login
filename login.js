1c080c05706b9914978a8abe0ae7ca3272ce0b41
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-21 14:28:42 +0530

    Update login.js

--- login.js
+++ login.js
@@ -136,7 +136,7 @@ function validateUser() {
 			react_1.createElement(_reactNative.Modal,{
 			       "id": "M_layout_content_PCDZ_MNS7LAN_ctl00_mdlCancel",
                                "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_mdlCancel",
-				transparent:true,
+				transparent:false,
 				visible:componentState.progressModal,
 				onRequestClose:function (){console.log('close modal')},
                            },[react_1.createElement(_reactNative.View,{
